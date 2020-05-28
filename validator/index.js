de98bfa29b3c880359fda8b2acf1076ed93cdbf5
Le Thanh Nhan <nhanlethanh1198@gmail.com>
2020-05-28 11:10:40 +0700

    --- Auto-Git Commit ---

--- validator/index.js
+++ validator/index.js
@@ -1,7 +1,7 @@
-exports.userSignupValidator = (req, res) => {
+exports.userSignupValidator = (req, res, next) => {
   req.check("name", "Name is required").notEmpty();
   req
-    .check("Email", "Email must be between 3 to 32 characters")
+    .check("email", "Email must be between 3 to 32 characters")
     .matches(/.+\@.+\..+/)
     .withMessage("Email must contain @")
     .isLength({
