8367b86ecdb41133347509c5ce6e5e546d9b25b1
Le Thanh Nhan <nhanlethanh1198@gmail.com>
2020-05-31 13:08:35 +0700

    6. Node - Sending Products with Queries (Done Backend)

--- controllers/product.js
+++ controllers/product.js
@@ -150,15 +150,15 @@ exports.update = (req, res) => {
 
 /**
  * sell / arrival
- * by sell = /product?sortBy=sold&order=desc&limit=4
- * by arrival = /product?sortBy=createAt&order=desc&limit=4
+ * by sell = /products?sortBy=sold&order=desc&limit=4
+ * by arrival = /products?sortBy=createdAt&order=desc&limit=4
  * if no param are send, then all products are return
  */
 
 exports.list = (req, res) => {
 	let order = req.query.order ? req.query.order : "asc";
 	let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
-	let limit = req.query.limit ? req.query.limit : 6;
+	let limit = req.query.limit ? parseInt(req.query.limit) : 6;
 
 	Product.find()
 		.select("-photo")
@@ -173,3 +173,76 @@ exports.list = (req, res) => {
 			res.send(data);
 		});
 };
+
+// list categories
+exports.listCategories = (req, res) => {
+	Product.distinct("category", {}, (error, categories) => {
+		if (error)
+			return res.status(400).json({
+				error: "Products not found!",
+			});
+		res.json(categories);
+	});
+};
+
+
+
+/**
+ * list products by search
+ * we will implement product search in react frontend
+ * we will show categories in checkbox and price range in radio buttons
+ * as the user clicks on those checkbox and radio buttons
+ * we will make api request and show the products to users based on what he wants
+ */
+
+exports.listBySearch = (req, res) => {
+    let order = req.body.order ? req.body.order : "desc";
+    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = parseInt(req.body.skip);
+    let findArgs = {};
+
+    // console.log(order, sortBy, limit, skip, req.body.filters);
+    // console.log("findArgs", findArgs);
+
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                // gte -  greater than price [0-10]
+                // lte - less than
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                };
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+
+    Product.find(findArgs)
+        .select("-photo")
+        .populate("category")
+        .sort([[sortBy, order]])
+        .skip(skip)
+        .limit(limit)
+        .exec((error, data) => {
+            if (error) {
+                return res.status(400).json({
+                    error: "Products not found"
+                });
+            }
+            res.json({
+                size: data.length,
+                data
+            });
+        });
+};
+
+exports.photo = (req, res, next) => {
+	if(req.product.photo.data){
+		res.set('Content-Type', req.product.photo.contentType)
+		res.send(req.product.photo.data)
+	}
+	next();
+}
--- routes/product.js
+++ routes/product.js
@@ -7,13 +7,15 @@ const {
 	read,
 	remove,
 	update,
-	list
+	list,
+	listCategories,
+	listBySearch,
+	photo
 } = require("../controllers/product");
 const { userById } = require("../controllers/user");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
-router.get("/product/:productId", read);
 router.delete(
 	"/product/:productId/:userId",
 	requireSignin,
@@ -28,7 +30,11 @@ router.put(
 	isAdmin,
 	update
 );
-router.get("/products", list)
+router.get("/product/:productId", read);
+router.get("/products", list);
+router.get("/products/categories", listCategories);
+router.post("/products/by/search", listBySearch);
+router.get("/product/photo/:productId", photo);
 
 router.param("userId", userById);
 router.param("productId", productById);
